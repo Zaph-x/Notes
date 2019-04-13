de10991af5c105dc7ca9299e6a8ce92ed2505a41
Zaph-x <mikkel@luceartfoto.dk>
2019-04-14 00:09:02 +0200

    Added Abstraction

---
+++ SLIAL/labs/fields/abstraction.py
@@ -0,0 +1,8 @@
+def solve(a,b,c): # Solve for x in ax+b=c
+    return (c-b)/a
+
+
+print(solve(10,5,6)) # 0.1
+print(solve(-3,2,9)) # -2.333->
+print(solve(4+2j,7,3)) # (-0.8+0.4j)
+print(solve(0,23,68)) # Error
