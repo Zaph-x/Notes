18cb0dd4795a55e0d6a8c702b0d24f096fd2d0e1
Zaph-x <mikkel@luceartfoto.dk>
2019-04-13 22:17:50 +0200

    Finished scalar

--- SLIAL/labs/vectors/scalar_plot.py
+++ SLIAL/labs/vectors/scalar_plot.py
@@ -4,5 +4,5 @@ import plotting as plot
 def scalar_vec_mult(a, v): # Function to multiply vector
     return [a*v[i] for i in range(len(v))]
 
-plot.plot([scalar_vec_mult(i/100, [3,2]) for i in range(101)], 5) # plot image
+plot.plot([scalar_vec_mult(i/100, [3,2]) for i in range(-301,301)], 5) # plot image
 time.sleep(1) # Pause to avoid prematurely deleting file
